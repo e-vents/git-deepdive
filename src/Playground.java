9ef50b7950d08933ce5d9b10b37f484fac7c7eff
e-vents <phipu@windowslive.com>
2021-10-16 09:55:21 +0200

    first commit

---
+++ src/Playground.java
@@ -0,0 +1,9 @@
+public class Playground {
+
+  public static void main(String[] args) {
+    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int value : numbers) {
+      System.out.print(value + " ");
+    }
+  }
+}
