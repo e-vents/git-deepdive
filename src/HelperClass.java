3cb062311e3beb1053e6cafdd44172b520ec4191
e-vents <phipu@windowslive.com>
2021-10-16 10:27:21 +0200

    first commit of seconds feature

--- src/HelperClass.java
+++ src/HelperClass.java
@@ -1,13 +1,17 @@
 public class HelperClass {
 
   int day;
+  int month;
 
   public HelperClass() {
     day = 2;
+    month = 12;
   }
 
+  // unusual but needed
   public void unusualChange() {
     day = 3;
+    month = 10;
   }
 
   public void unusualChangeSecond() {
